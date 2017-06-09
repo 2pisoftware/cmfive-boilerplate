14be235195be04fc44ffc53251fe32e7dbb0739e
Adam Buckley <adam@2pisoftware.com>
2017-06-09 12:12:33 +1000

    Have working project based composer install via cmfive.php build script

--- cmfive.php
+++ cmfive.php
@@ -1,14 +1,58 @@
 #!/bin/php
 <?php
 
-if ($argc < 2) {
-	die('Missing command');
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+$composer_string = <<<COMPOSER
+{
+    "name": "cmfive-boilerplate",
+    "version": "1.0",
+    "description": "A boilerplate project layout for Cmfive",
+    "require": {
+        "2pisoftware/cmfive-core": "dev-master"
+    },
+	"config": {
+        "vendor-dir": "composer/vendor",
+        "cache-dir": "composer/cache",
+        "bin-dir": "composer/bin"
+    },
+    "repositories": [
+        {
+            "type":"package",
+            "package": {
+            "name": "2pisoftware/cmfive-core",
+            "version":"master",
+            "source": {
+                "url": "https://github.com/2pisoftware/cmfive-core",
+                "type": "git",
+                "reference":"develop"
+                }
+            }
+        }
+    ]
 }
+COMPOSER;
+//,
+//    "scripts": {
+//        
+//    }
+//"post-install-cmd": [
+//            "php -r \"copy('composer/vendor/2pisoftware/cmfive-core/system/', 'system/');\"",
+//            "php cmfive.php install",
+//            "composer update"
+//        ]
+$composer_json = json_decode($composer_string, true);
+
+file_put_contents('./composer.json', json_encode($composer_json,JSON_PRETTY_PRINT));
+
+echo exec('php composer.phar install');
+
+echo exec('cp -r composer/vendor/2pisoftware/cmfive-core/system system');
 
 require('system/web.php');
 $w = new Web();
 
-$composer_json = json_decode(file_get_contents('./composer.json'), true);
 $dependencies_array = array();
 foreach($w->modules() as $module) {
 	$dependencies = Config::get("{$module}.dependencies");
@@ -18,9 +62,7 @@ foreach($w->modules() as $module) {
 }
 
 $composer_json['require'] = array_merge($composer_json['require'], $dependencies_array);
+file_put_contents('./composer.json', json_encode($composer_json,JSON_PRETTY_PRINT));
 
-file_put_contents('./composer.json', json_encode($composer_json));
-
-echo "composer.json updated";
-
+echo exec('php composer.phar update');
 exit(0);
