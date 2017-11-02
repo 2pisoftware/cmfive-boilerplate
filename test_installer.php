79f4115cab23adbe73576daec8bf49753777456b
isaac <iceaxe.liehne@gmail.com>
2017-11-02 12:03:52 +1100

    debugging

--- test_installer.php
+++ test_installer.php
@@ -6,6 +6,7 @@ echo exec('mkdir test_runner');
 echo exec('cd test_runner');
 echo "Cloning testrunner...\n\n";
 echo exec('git clone https://iceaxelihne@bitbucket.org/2pisoftware/selenium-testrunner.git');
+echo exec('ls -l /../');
 echo "Running tests...\n\n";
 echo exec('sudo php slenium-testrunner/test_runner.php /.. http://localhost');
 
