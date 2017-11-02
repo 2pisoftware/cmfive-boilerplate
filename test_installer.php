28388bde2fa9aa4be9ee92337c0bed5a217b1280
isaac <iceaxe.liehne@gmail.com>
2017-11-02 12:13:10 +1100

    trying to find the testrunner

--- test_installer.php
+++ test_installer.php
@@ -2,10 +2,10 @@
 
 
 echo "Cloning testrunner...\n\n";
-echo exec('git clone https://iceaxelihne@bitbucket.org/2pisoftware/selenium-testrunner.git');
+echo exec('git clone https://iceaxelihne@bitbucket.org/2pisoftware/selenium-testrunner.git iceaxeliehne/selenium-testrunner');
 echo exec('ls -l');
 echo "Running tests...\n\n";
-echo exec('sudo php slenium-testrunner/test_runner.php /iceaxeliehne/cmfive-boilerplate/ http://localhost');
+echo exec('sudo php iceaxeliehne/selenium-testrunner/test_runner.php /iceaxeliehne/cmfive-boilerplate/ http://localhost');
 
 
 ?>
