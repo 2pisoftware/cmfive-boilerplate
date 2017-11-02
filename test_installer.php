5cced6db5fe9ea59c080be323f3f92d87bf20ae6
isaac <iceaxe.liehne@gmail.com>
2017-11-02 11:56:57 +1100

    added comments

--- test_installer.php
+++ test_installer.php
@@ -1,9 +1,13 @@
 <?php
 
+echo "making testrunner dir...\n\n";
 echo exec('mkdir test_runner');
+
 echo exec('cd test_runner');
+echo "Cloning testrunner...\n\n";
 echo exec('git clone https://iceaxelihne@bitbucket.org/2pisoftware/selenium-testrunner.git');
-echo exec('sudo php test_runner.php /.. http://localhost');
+echo "Running tests...\n\n";
+echo exec('sudo php slenium-testrunner/test_runner.php /.. http://localhost');
 
 
 ?>
