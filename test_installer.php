569c4e29f197a6bea24f36897c22ed69c5620e43
isaac <iceaxe.liehne@gmail.com>
2017-11-02 13:17:22 +1100

    more debugs

--- test_installer.php
+++ test_installer.php
@@ -7,6 +7,9 @@ echo "moving to repo directory...\n\n";
 echo exec('cd iceaxeliehne/selenium-testrunner');
 echo exec('ls');
 echo "\n\n";
+require_once("Testrunner_class.php");
+Testrunner::output("testrunner found", Testrunner::SUCCESS);
+echo "\n\n";
 echo "Running tests...\n\n";
 echo exec('sudo php test_runner.php ../cmfive-boilerplate/ http://localhost:8000');
 
