b34603d14771fcb9513443ded733849fee4baeb2
isaac <iceaxe.liehne@gmail.com>
2017-11-02 11:28:39 +1100

    added params to cmfive.php call and run tests

--- test_installer.php
+++ test_installer.php
@@ -1,6 +1,9 @@
 <?php
 
+echo exec('mkdir test_runner');
+echo exec('cd test_runner');
 echo exec('git clone https://iceaxelihne@bitbucket.org/2pisoftware/selenium-testrunner.git');
+echo exec('sudo php test_runner.php /.. http://localhost');
 
 
 ?>
