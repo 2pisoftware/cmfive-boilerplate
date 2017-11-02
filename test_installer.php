ccc67bd1649d7e4ff890452288ffa61b96e62c38
isaac <iceaxe.liehne@gmail.com>
2017-11-02 13:47:46 +1100

    added testrunner params

--- test_installer.php
+++ test_installer.php
@@ -5,13 +5,13 @@ echo "Cloning testrunner...\n\n";
 echo exec('git clone https://iceaxelihne@bitbucket.org/2pisoftware/selenium-testrunner.git selenium-testrunner');
 echo "moving to repo directory...\n\n";
 chdir('selenium-testrunner');
-echo exec('ls ~/build/iceaxeliehne/');
+echo exec('ls');
 echo "\n\n";
 require_once("Testrunner_class.php");
 Testrunner::output("testrunner found", Testrunner::SUCCESS);
 echo "\n\n";
 echo "Running tests...\n\n";
-echo exec('sudo php test_runner.php ~/build/iceaxeliehne/cmfive-boilerplate/ http://cmfive.dev');
+echo exec('php test_runner.php ~/build/iceaxeliehne/cmfive-boilerplate/ http://cmfive.dev false isaac@2pisoftware.com');
 echo "finshed...\n\n";
 
 
