5080f4894194e3ec890d75bc75170a02e54dde6e
isaac <iceaxe.liehne@gmail.com>
2017-11-02 12:08:50 +1100

    trying to get testrunner to run

--- test_installer.php
+++ test_installer.php
@@ -1,14 +1,11 @@
 <?php
 
-echo "making testrunner dir...\n\n";
-echo exec('mkdir test_runner');
 
-echo exec('cd test_runner');
 echo "Cloning testrunner...\n\n";
 echo exec('git clone https://iceaxelihne@bitbucket.org/2pisoftware/selenium-testrunner.git');
-echo exec('ls -l /../');
+echo exec('ls -l');
 echo "Running tests...\n\n";
-echo exec('sudo php slenium-testrunner/test_runner.php /.. http://localhost');
+echo exec('sudo php slenium-testrunner/test_runner.php /iceaxeliehne/cmfive-boilerplate/ http://localhost');
 
 
 ?>
