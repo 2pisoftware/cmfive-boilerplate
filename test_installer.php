aad71807954f7387044c5f0da056e114b87414cd
isaac <iceaxe.liehne@gmail.com>
2017-11-02 13:33:02 +1100

    testing for correct path

--- test_installer.php
+++ test_installer.php
@@ -5,13 +5,13 @@ echo "Cloning testrunner...\n\n";
 echo exec('git clone https://iceaxelihne@bitbucket.org/2pisoftware/selenium-testrunner.git selenium-testrunner');
 echo "moving to repo directory...\n\n";
 chdir('selenium-testrunner');
-echo exec('ls /iceaxeliehne/');
+echo exec('ls ~/iceaxeliehne/');
 echo "\n\n";
 require_once("Testrunner_class.php");
 Testrunner::output("testrunner found", Testrunner::SUCCESS);
 echo "\n\n";
 echo "Running tests...\n\n";
-echo exec('sudo php test_runner.php /iceaxeliehne/cmfive-boilerplate/ http://localhost:8000');
+echo exec('sudo php test_runner.php ~/iceaxeliehne/cmfive-boilerplate/ http://localhost:8000');
 
 
 ?>
