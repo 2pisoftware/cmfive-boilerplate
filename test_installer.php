8baac3d6d06c8826e7f1c4a9868b6141385d2d32
isaac <iceaxe.liehne@gmail.com>
2017-11-02 12:41:55 +1100

    now looking for site dir

--- test_installer.php
+++ test_installer.php
@@ -5,7 +5,8 @@ echo "Cloning testrunner...\n\n";
 echo exec('git clone https://iceaxelihne@bitbucket.org/2pisoftware/selenium-testrunner.git iceaxeliehne/selenium-testrunner');
 echo "moving to repo directory...\n\n";
 echo exec('cd iceaxeliehne/selenium-testrunner');
-echo exec('ll ../');
+echo exec('ls -l ../');
+echo "\n\n";
 echo "Running tests...\n\n";
 echo exec('sudo php test_runner.php ../cmfive-boilerplate/ http://localhost:8000');
 
