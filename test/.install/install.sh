c46c5ea167b642fc80f0bdadaa16459d148d5fb8
DerekCrannaford <derek@tripleacs.com>
2020-08-24 20:31:46 +1000

    Docker volume and Codecept_Composer installer

---
+++ test/.install/install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#setup for Codeception
+
+cd /var/www/html/test/Codeception/ ;
+
+export  COMPOSER_ALLOW_SUPERUSER=1 ;
+curl -sS https://getcomposer.org/installer | php -- \
+        --filename=composer \
+        --install-dir=. ;
+
+./composer -q require codeception/codeception --dev ;
+
+./composer require --no-update \
+codeception/module-apc \
+codeception/module-asserts \
+codeception/module-cli \
+codeception/module-db \
+codeception/module-filesystem \
+codeception/module-ftp \
+codeception/module-memcache \
+codeception/module-mongodb \
+codeception/module-phpbrowser \
+codeception/module-redis \
+codeception/module-rest \
+codeception/module-sequence \
+codeception/module-soap \
+codeception/module-webdriver && \
+./composer update --no-dev --prefer-dist --no-interaction --optimize-autoloader --apcu-autoloader;
+
+vendor/bin/codecept bootstrap ;
+
+chmod -R 775 tests/* ;
+
+
