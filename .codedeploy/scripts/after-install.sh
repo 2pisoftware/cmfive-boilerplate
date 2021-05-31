eae0cba2ce3ac9caded5e3bee506085b3d43ed14
Taliesin Millhouse <taliesinwrmillhouse@gmail.com>
2021-05-31 19:33:48 +1000

    fix: trying fix

--- .codedeploy/scripts/after-install.sh
+++ .codedeploy/scripts/after-install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+cd /var/www/cmfive-boilerplate
 chown -R ubuntu:ubuntu $(pwd)
--- .codedeploy/scripts/application-start.sh
+++ .codedeploy/scripts/application-start.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-cd ../..
+cd /var/www/cmfive-boilerplate
 docker-compose up -d
 docker exec -it $(docker-compose ps -q webapp) /bin/bash
 ./composer.phar self-update --2
