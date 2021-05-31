e31d7d76e404f933e6cf01abcac009b1bd84b96c
Taliesin Millhouse <taliesinwrmillhouse@gmail.com>
2021-06-01 08:09:21 +1000

    fix: updated scripts

--- .codedeploy/scripts/application-start.sh
+++ .codedeploy/scripts/application-start.sh
@@ -2,14 +2,3 @@
 
 cd /var/www/cmfive-boilerplate
 docker-compose up -d
-
-docker exec -it $(docker-compose ps -q webapp) ./composer.phar self-update --2
-
-# Temp fix, remove once Config::setFromS3Object is in master.
-composer/vendor/2pisoftware/cmfive-core
-git checkout feature/ConfigSetFromS3
-cd ../../../..
-
-docker exec -it $(docker-compose ps -q webapp) php cmfive.php install core
-docker exec -it $(docker-compose ps -q webapp) php cmfive.php seed encryption
-docker exec -it $(docker-compose ps -q webapp) php cmfive.php install migrations
