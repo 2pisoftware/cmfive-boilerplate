a32893b08a88ddd9ba8bc953c42d056be5c122cd
Taliesin Millhouse <taliesinwrmillhouse@gmail.com>
2021-05-31 20:12:05 +1000

    fix: trying fix

--- .codedeploy/scripts/application-stop.sh
+++ .codedeploy/scripts/application-stop.sh
@@ -1,6 +1,11 @@
 #/bin/bash
 
+pwd >> /home/ubuntu/temp.txt
+ls >> /home/ubuntu/temp1.txt
+
 if [ -d "/var/www/cmfive-boilerplate" ]; then
     cd /var/www/cmfive-boilerplate
+    pwd >> /home/ubuntu/temp2.txt
+    ls >> /home/ubuntu/temp3.txt
     docker-compose down
 fi
