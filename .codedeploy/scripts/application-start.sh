ebc04dd0c3fa0ced6ee97b26a38c9ca96d69f30c
Taliesin Millhouse <taliesinwrmillhouse@gmail.com>
2021-06-01 09:22:10 +1000

    fix: added command back

--- .codedeploy/scripts/application-start.sh
+++ .codedeploy/scripts/application-start.sh
@@ -12,3 +12,5 @@ docker exec -it $(cd /var/www/cmfive-boilerplate && docker-compose ps -q webapp)
 cd composer/vendor/2pisoftware/cmfive-core
 sudo git checkout feature/ConfigSetFromS3
 cd ../../../..
+
+docker exec -it $(cd /var/www/cmfive-boilerplate && docker-compose ps -q webapp) php cmfive.php install core
