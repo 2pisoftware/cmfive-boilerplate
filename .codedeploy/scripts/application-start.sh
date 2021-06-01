b6eca68e60e1cca392caaa9448cf361d35d712f5
Taliesin Millhouse <taliesinwrmillhouse@gmail.com>
2021-06-01 17:49:08 +1000

    fix: updated script

--- .codedeploy/scripts/application-start.sh
+++ .codedeploy/scripts/application-start.sh
@@ -3,3 +3,19 @@
 cd /var/www/cmfive-boilerplate
 docker-compose down
 docker-compose up -d
+
+# Update to Composer V2.
+docker exec $(cd /var/www/cmfive-boilerplate && docker-compose ps -q webapp) ./composer.phar self-update --2
+# Add the AWS SDK so the config can be loaded from S3.
+docker exec $(cd /var/www/cmfive-boilerplate && docker-compose ps -q webapp) ./composer.phar require "aws/aws-sdk-php:^3.24"
+docker exec $(cd /var/www/cmfive-boilerplate && docker-compose ps -q webapp) php cmfive.php install core
+docker exec $(cd /var/www/cmfive-boilerplate && docker-compose ps -q webapp) ./composer.phar require "aws/aws-sdk-php:^3.24"
+docker exec $(cd /var/www/cmfive-boilerplate && docker-compose ps -q webapp) ./composer.phar update
+docker exec $(cd /var/www/cmfive-boilerplate && docker-compose ps -q webapp) php cmfive.php install core
+
+docker exec $(cd /var/www/cmfive-boilerplate && docker-compose ps -q webapp) php cmfive.php seed encryption
+# Run it twice for some reason?
+docker exec $(cd /var/www/cmfive-boilerplate && docker-compose ps -q webapp) php cmfive.php install migrations
+docker exec $(cd /var/www/cmfive-boilerplate && docker-compose ps -q webapp) php cmfive.php install migrations
+
+chown -R ubuntu:ubuntu $(pwd)
