c82af5929e28186bc742a2e7e6c433cbaf348874
Taliesin Millhouse <taliesinwrmillhouse@gmail.com>
2021-06-05 08:00:45 +1000

    fix: path issue

--- .codepipeline/codedeploy/application-start.sh
+++ .codepipeline/codedeploy/application-start.sh
@@ -1,7 +1,10 @@
 #/bin/bash
 
+sudo apt update
+sudo apt upgrade -y
+
 cd /var/www/cmfive-boilerplate
 docker-compose up -d
 docker exec $(cd /var/www/cmfive-boilerplate && docker-compose ps -q webapp) php cmfive.php install core
-docker exec $(cd /var/www/cmfive-boilerplate && docker-compose ps -q webapp) chown -R www-data:www-data $(pwd)
+docker exec $(cd /var/www/cmfive-boilerplate && docker-compose ps -q webapp) chown -R www-data:www-data /var/www/html
 docker exec $(cd /var/www/cmfive-boilerplate && docker-compose ps -q webapp) php cmfive.php install migrations
