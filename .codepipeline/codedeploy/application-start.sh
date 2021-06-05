c5e42793c9838e9f43c4f634fd9e7b2dab62280c
Taliesin Millhouse <taliesinwrmillhouse@gmail.com>
2021-06-05 10:53:28 +1000

    fix: permissions issue

--- .codepipeline/codedeploy/application-start.sh
+++ .codepipeline/codedeploy/application-start.sh
@@ -5,6 +5,8 @@ sudo apt upgrade -y
 
 cd /var/www/cmfive-boilerplate
 docker-compose up -d
+docker exec $(cd /var/www/cmfive-boilerplate && docker-compose ps -q webapp) mkdir /var/www/html/storage/log
+docker exec $(cd /var/www/cmfive-boilerplate && docker-compose ps -q webapp) mkdir /var/www/html/storage/session
 docker exec $(cd /var/www/cmfive-boilerplate && docker-compose ps -q webapp) php cmfive.php install core
 docker exec $(cd /var/www/cmfive-boilerplate && docker-compose ps -q webapp) chown -R www-data:www-data /var/www/html
 docker exec $(cd /var/www/cmfive-boilerplate && docker-compose ps -q webapp) php cmfive.php install migrations
