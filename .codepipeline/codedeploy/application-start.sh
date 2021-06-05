c8dbfceb5358c80210f0037de892ca1ad3a9cf98
Taliesin Millhouse <taliesinwrmillhouse@gmail.com>
2021-06-05 21:50:07 +1000

    fix: small order change

--- .codepipeline/codedeploy/application-start.sh
+++ .codepipeline/codedeploy/application-start.sh
@@ -8,8 +8,8 @@ docker-compose up -d
 docker exec $(cd /var/www/cmfive-boilerplate && docker-compose ps -q webapp) mkdir /var/www/html/storage/log
 docker exec $(cd /var/www/cmfive-boilerplate && docker-compose ps -q webapp) mkdir /var/www/html/storage/session
 docker exec $(cd /var/www/cmfive-boilerplate && docker-compose ps -q webapp) php cmfive.php install core
-docker exec $(cd /var/www/cmfive-boilerplate && docker-compose ps -q webapp) chown -R www-data:www-data /var/www/html
 docker exec $(cd /var/www/cmfive-boilerplate && docker-compose ps -q webapp) php cmfive.php install migrations
+docker exec $(cd /var/www/cmfive-boilerplate && docker-compose ps -q webapp) chown -R www-data:www-data /var/www/html
 
 if [ -f "cache/classdirectory.cache" ]; then
     sudo rm cache/classdirectory.cache
