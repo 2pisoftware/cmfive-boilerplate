5fbe9a6a682638da4411b0f66b5b9ebd190398e8
Taliesin Millhouse <taliesinwrmillhouse@gmail.com>
2021-09-22 21:46:28 +0000

    fix: after install script

--- .codepipeline/codedeploy/after-install.sh
+++ .codepipeline/codedeploy/after-install.sh
@@ -5,10 +5,10 @@ echo "Running After Install"
 cd /var/www/cmfive-boilerplate
 
 # Copy config files.
-cp .codepipeline/configs/fpm/* /etc/php/7.4/fpm/
-cp .codepipeline/configs/nginx/nginx.conf /etc/nginx/nginx.conf
-cp .codepipeline/configs/nginx/default.conf /etc/nginx/conf.d/default.conf
-cp .codepipeline/configs/supervisord.conf /etc/supervisord.conf
+cp codepipeline/configs/fpm/* /etc/php/7.4/fpm/
+cp codepipeline/configs/nginx/nginx.conf /etc/nginx/nginx.conf
+cp codepipeline/configs/nginx/default.conf /etc/nginx/conf.d/default.conf
+cp codepipeline/configs/supervisord.conf /etc/supervisord.conf
 
 # Run migrations.
 php cmfive.php install migrations
