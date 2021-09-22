709b4d0be96f003e18edc047be88dcb2aa7070c7
Taliesin Millhouse <taliesinwrmillhouse@gmail.com>
2021-09-22 03:50:37 +0000

    fix: buildspec scripts

--- .codepipeline/codedeploy/after-install.sh
+++ .codepipeline/codedeploy/after-install.sh
@@ -2,6 +2,11 @@
 
 echo "Running After Install"
 
+# Copy config files.
+cp .codepipeline/configs/fpm/* /etc/php/7.4/fpm/
+cp .codepipeline/configs/nginx/nginx.conf /etc/nginx/nginx.conf
+cp .codepipeline/configs/nginx/default.conf /etc/nginx/conf.d/default.conf
+cp .codepipeline/configs/supervisord.conf /etc/supervisord.conf
+
 # Run migrations.
-cd /var/www/cmfive-boilerplate
 php cmfive.php install migrations
--- .codepipeline/codedeploy/before-install.sh
+++ .codepipeline/codedeploy/before-install.sh
@@ -7,9 +7,3 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt autoclean
 sudo apt autoremove -y
-
-# Copy config files.
-cp .codepipeline/configs/fpm/* /etc/php/7.4/fpm/
-cp .codepipeline/configs/nginx/nginx.conf /etc/nginx/nginx.conf
-cp .codepipeline/configs/nginx/default.conf /etc/nginx/conf.d/default.conf
-cp .codepipeline/configs/supervisord.conf /etc/supervisord.conf
