bb5971419f9f363b5145699fb0a8f87b9b1a7bba
Taliesin Millhouse <taliesinwrmillhouse@gmail.com>
2021-09-23 04:01:32 +0000

    ci: after install script

--- .codepipeline/codedeploy/after-install.sh
+++ .codepipeline/codedeploy/after-install.sh
@@ -9,12 +9,12 @@ cd /var/www/html
 # Create symlink to system.
 sudo ln -s /var/www/html/composer/vendor/2pisoftware/cmfive-core/system /var/www/html/system
 
-# Update permissions.
-sudo chown -R www-data:www-data .
-
 # Run migrations.
 php cmfive.php install migrations
 
 # Clear the cache.
 sudo rm -f cache/config.cache
 sudo rm -f cache/classdirectory.cache
+
+# Update permissions.
+sudo chown -R www-data:www-data .
