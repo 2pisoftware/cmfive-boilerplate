273b111e3b26a0279bad4005ecf9bb45d68d8cbd
Taliesin Millhouse <taliesinwrmillhouse@gmail.com>
2021-10-11 15:15:37 +1100

    Delete xdebug-fix.sh

--- xdebug-fix.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-while read a; do
-    echo ${a//docker.for.mac.localhost/172.18.0.1}
-done < /etc/php/7.4/fpm/conf.d/20-xdebug.ini > /etc/php/7.4/fpm/conf.d/20-xdebug.ini.t
-mv /etc/php/7.4/fpm/conf.d/20-xdebug.ini{.t,}
-
-service php7.4-fpm restart
