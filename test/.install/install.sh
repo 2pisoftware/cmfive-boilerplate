c9927feac633c4ff1276ebcd85acb9bcd58deb1d
Adam Buckley <adam.buckley90@gmail.com>
2022-05-04 12:07:48 +1000

    Update install.sh

--- test/.install/install.sh
+++ test/.install/install.sh
@@ -13,9 +13,9 @@ curl -sS https://getcomposer.org/installer | php -- \
 ./composer config minimum-stability dev
 ./composer -q require codeception/codeception:4.1.31 --dev ;
 
-./composer require --no-update codeception/module-asserts:3.0.0 \
+./composer require --no-update codeception/module-asserts:2.0.1 \
 codeception/module-db:3.0.1 \
-codeception/module-webdriver:3.1.1 \
+codeception/module-webdriver:2.0.2 \
 codeception/module-phpbrowser:2.0.2
 
 ./composer update --prefer-source --no-interaction --no-progress --optimize-autoloader --ansi;
