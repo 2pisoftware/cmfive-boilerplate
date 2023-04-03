6cf2b02d012520fb459616fcb512b0ec5864a154
Derek Crannaford <derek@2pisoftware.com>
2023-04-03 20:33:55 +1000

    Still tweaking XDB

--- .codepipeline/local-dev/start.sh
+++ .codepipeline/local-dev/start.sh
@@ -14,7 +14,6 @@ echo "zend_extension=xdebug.so" > /etc/php/8.1/fpm/conf.d/20-xdebug.ini \
 	&& echo "xdebug.client_port=9003" >> /etc/php/8.1/fpm/conf.d/20-xdebug.ini \
 	&& echo "xdebug.remote_connect_back=On" >> /etc/php/8.1/fpm/conf.d/20-xdebug.ini \
 	&& echo "xdebug.remote_handler=dbgp" >> /etc/php/8.1/fpm/conf.d/20-xdebug.ini \
-	&& echo "xdebug.start_with_request=yes" >> /etc/php/8.1/fpm/conf.d/20-xdebug.ini \
 	&& echo "xdebug.remote_log=/tmp/xdebug.log" >> /etc/php/8.1/fpm/conf.d/20-xdebug.ini \
 	&& echo "xdebug.max_nesting_level=250" >> /etc/php/8.1/fpm/conf.d/20-xdebug.ini \
 
