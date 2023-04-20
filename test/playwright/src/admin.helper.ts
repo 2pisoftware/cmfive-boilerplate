25e2b9383162826141e05f3a11debecc691b10f9
Claire Smith <claire@2pisoftware.com>
2023-04-20 11:12:04 +1000

    Adding CmfiveHelper file

---
+++ test/playwright/src/admin.helper.ts
@@ -0,0 +1,236 @@
+import { expect, Page } from "@playwright/test";
+import { HOST, CmfiveHelper } from "./cmfive.helper";
+
+export class AdminHelper {
+    static async createUser(page: Page, username: string, password: string, firstname: string, lastname: string, email: string, permissions: string[] = [])
+    {
+        await CmfiveHelper.clickCmfiveNavbar(page, "Admin", "List Users");
+        await page.waitForURL(HOST + "/admin/users#internal");
+
+        await page.getByRole("button", {name: "Add New User"}).click();
+
+        await page.waitForSelector("#cmfive-modal", {state: "visible"});
+
+        await page.getByLabel("Active", {exact: true}).check();
+        await page.getByLabel("Login Required").fill(username);
+        await page.getByLabel("Password", {exact: true}).fill(password);
+        await page.getByLabel("Repeat Password").fill(password);
+        await page.getByLabel("First Name").fill(firstname);
+        await page.getByLabel("Last Name").fill(lastname);
+        await page.getByLabel("Email").fill(email);
+
+
+        if(permissions.length == 0)
+            permissions.push("user");
+        
+        for(let permission of permissions)
+            await page.getByText(permission, {exact: true}).check();
+
+        await page.getByRole("button", {name: "Save"}).click();
+
+        await expect(page.getByText("User " + username + " added")).toBeVisible();
+    }
+
+    static async deleteUser(page: Page, username: string)
+    {
+        await CmfiveHelper.clickCmfiveNavbar(page, "Admin", "List Users");
+        await page.waitForURL(HOST + "/admin/users#internal");
+
+        await CmfiveHelper.getRowByText(page, username).getByRole("button", {name: "Remove"}).click();
+        await page.getByRole("button", {name: "Delete user"}).click();
+    
+        await expect(page.getByText("User " + username + " deleted.")).toBeVisible();
+    }
+
+    static async editUser(page: Page, username: string, data: [string, string][])
+    {
+        await CmfiveHelper.clickCmfiveNavbar(page, "Admin", "List Users");
+        await page.waitForURL(HOST + "/admin/users#internal");
+
+        await CmfiveHelper.getRowByText(page, username).getByRole("button", {name: "Edit"}).click();
+
+        for(let [label, value] of data)
+            await page.getByLabel(label, {exact: true}).fill(value);
+
+        await page.getByRole("button", {name: "Update"}).click();
+
+        await page.waitForEvent("requestfinished");
+        await expect(page.getByText("Account details updated").nth(3)).toBeVisible();
+    }
+
+    static async createLookupType(page: Page, type: string, code: string, lookup: string)
+    {
+        await CmfiveHelper.clickCmfiveNavbar(page, "Admin", "Lookup");
+        await page.getByRole("link", {name: "New Item", exact: true}).click();
+
+        await page.getByLabel("or Add New Type").fill(type);
+        await page.getByLabel("Code").fill(code);
+        await page.getByLabel("Title").fill(lookup);
+        await page.getByRole("button", {name: "Save"}).click();
+
+        await expect(page.getByText("Lookup Item added")).toBeVisible();
+    }
+
+    static async createLookup(page: Page, type: string, code: string, lookup: string)
+    {
+        await CmfiveHelper.clickCmfiveNavbar(page, "Admin", "Lookup");
+        await page.getByRole("link", {name: "New Item", exact: true}).click();
+
+        await page.getByLabel("Type").selectOption(type);
+        await page.getByLabel("Code").fill(code);
+        await page.getByLabel("Title").fill(lookup);
+        await page.getByRole("button", {name: "Save"}).click();
+
+        await expect(page.getByText("Lookup Item added")).toBeVisible();
+    }
+
+    static async deleteLookup(page: Page, lookup: string)
+    {
+        await CmfiveHelper.clickCmfiveNavbar(page, "Admin", "Lookup");
+
+        await CmfiveHelper.getRowByText(page, lookup).getByRole("button", {name: "Delete"}).click();
+    }
+
+    static async editLookup(page: Page, lookup: string, data: Record<string, string>)
+    {
+        await CmfiveHelper.clickCmfiveNavbar(page, "Admin", "Lookup");
+        await CmfiveHelper.getRowByText(page, lookup).getByRole("button", {name: "Edit"}).click();
+        await page.waitForSelector("#cmfive-modal", {state: "visible"});
+
+        if(data["Type"] != undefined)
+            await page.getByLabel("Type").selectOption(data["Type"]);
+
+        if(data["Code"] != undefined)
+            await page.getByLabel("Code").fill(data["Code"]);
+        
+        if(data["Title"] != undefined)
+            await page.getByLabel("Title").fill(data["Title"]);
+
+        await page.getByRole("button", {name: "Update"}).click();
+
+        await expect(page.getByText("Lookup Item edited")).toBeVisible();
+    }
+
+    static async createUserGroup(page: Page, usergroup: string)
+    {
+        await CmfiveHelper.clickCmfiveNavbar(page, "Admin", "List Groups");
+        await page.getByRole("button", {name: "New Group"}).click();
+        await page.waitForSelector("#cmfive-modal", {state: "visible"});
+
+        await page.getByLabel("Group Title: Required").fill(usergroup);
+        await page.getByRole("button", {name: "Save"}).click();
+
+        await expect(page.getByText("New group added!")).toBeVisible();
+    }
+
+    static async deleteUserGroup(page: Page, name: string)
+    {
+        await CmfiveHelper.clickCmfiveNavbar(page, "Admin", "List Groups");
+
+        await CmfiveHelper.getRowByText(page, name).getByRole("button", {name: "Delete"}).click();
+
+        await expect(page.getByText("Group is deleted!")).toBeVisible();
+    }
+
+    static async addUserGroupMember(page: Page, usergroup: string, user: string, owner: boolean = false)
+    {
+        await CmfiveHelper.clickCmfiveNavbar(page, "Admin", "List Groups");
+        await CmfiveHelper.getRowByText(page, usergroup).getByRole("button", {name: "More Info"}).click();
+
+        await page.getByRole("button", {name: "New Member"}).click();
+        await page.waitForSelector("#cmfive-modal", {state: "visible"});
+
+        await page.getByLabel("Select Member: Required").selectOption(user);
+
+        if(owner)
+            await page.getByLabel("Owner").check();
+        else
+            await page.getByLabel("Owner").uncheck();
+
+        await page.getByRole("button", {name: "Save"}).click();
+
+        await expect(page.getByText(user).first()).toBeVisible();
+    }
+
+    static async deleteUserGroupMember(page: Page, usergroup: string, user: string)
+    {
+        await CmfiveHelper.clickCmfiveNavbar(page, "Admin", "List Groups");
+        await CmfiveHelper.getRowByText(page, usergroup).getByRole("button", {name: "More Info"}).click();
+
+        await CmfiveHelper.getRowByText(page, user).getByRole("link", {name: "Delete"}).click();
+
+        await expect(page.getByText("Member is deleted!")).toBeVisible();
+    }
+
+    static async editUserGroupPermissions(page: Page, usergroup: string, permissions: string[])
+    {
+        await CmfiveHelper.clickCmfiveNavbar(page, "Admin", "List Groups");
+        await CmfiveHelper.getRowByText(page, usergroup).getByRole("button", {name: "More Info"}).click();
+
+        await page.getByRole("button", {name: "Edit Permissions"}).click();
+
+        for(let permission of permissions)
+            await page.getByText(permission, {exact: true}).check();
+
+        await page.getByRole("button", {name: "Save"}).click();
+
+        await expect(page.getByText("Permissions are updated!")).toBeVisible();
+    }
+
+    static async createTemplate(page: Page, templateTitle: string, module: string, category: string, code: string[])
+    {
+        await CmfiveHelper.clickCmfiveNavbar(page, "Admin", "Templates");
+        await page.getByRole("button", {name: "Add Template"}).click();
+
+        await page.getByLabel("Active").check();
+        await page.getByLabel("Title").fill(templateTitle);
+        await page.getByLabel("Module").fill(module);
+        await page.getByLabel("Category").fill(category);
+        await page.getByRole("button", {name: "Save"}).click();
+        await expect(page.getByText("Template saved")).toBeVisible();
+
+        await page.getByRole("link", {name: "Template", exact: true}).click();
+        await page.locator("#template_title").fill(templateTitle);
+        
+        await page.locator(".CodeMirror-lines").click();
+
+        // code mirror auto completes open/closed html tags, so skip writing them
+        // typing <table> puts cursor on new line between <table> and </table>
+        // typing <tr>/<td> writes </tr>/</td> respectively on the same line, cursor before the closing tag
+
+        for(let line of code) {
+            if(line.indexOf("</") != -1) {    
+                await page.keyboard.type(line.split("</")[0]);
+
+                for(let i = 0; i < line.split("</")[1].length + 2; i++)
+                    await page.keyboard.press("ArrowRight");
+
+                await page.keyboard.press("Enter");
+                continue;
+            }
+
+            await page.keyboard.type(line);
+            await page.keyboard.press("Enter");
+        }
+        
+        await page.getByRole("button", {name: "Save"}).click();
+        await expect(page.getByText("Template saved")).toBeVisible();
+    }
+
+    static async demoTemplate(page: Page, templateTitle: string): Promise<Page>
+    {
+        await CmfiveHelper.clickCmfiveNavbar(page, "Admin", "Templates");
+        await CmfiveHelper.getRowByText(page, templateTitle).getByRole("button", {name: "Edit"}).click();
+
+        const templateID = page.url().split("/admin-templates/edit/")[1].split("#")[0];
+
+        await page.getByRole("link", {name: "Test Output"}).click();
+
+        const [pages] = await Promise.all([page.context().waitForEvent("page")]);
+        const tabs = pages.context().pages();
+        console.log(tabs.length);
+        await tabs[1].waitForLoadState("load");
+
+        return tabs[1];
+    }
+}
---
+++ test/playwright/src/cmfive.helper.ts
@@ -0,0 +1,68 @@
+import { Page, expect } from "@playwright/test";
+import { DateTime } from "luxon";
+
+export const HOST = "http://localhost:3000";
+export const GLOBAL_TIMEOUT = 600_000;
+
+export class CmfiveHelper {
+    static randomID = (prefix: string) => prefix + (Math.random() + 1).toString(36).substring(7)
+
+    static async login(page: Page, user: string, password: string)
+    {
+        await page.goto(HOST + "/auth/login");
+        await page.locator("#login").fill(user);
+        await page.locator("#password").fill(password);
+        await page.getByRole("button", {name: "Login"}).click();
+    }
+
+    static async logout(page: Page)
+    {
+        await page.goto(HOST + "/auth/logout");
+    }
+
+    static async isBootstrap5(page: Page): Promise<boolean>
+    {
+        return await page.locator("html.theme").count() == 1;
+    }
+
+    static getRowByText(page: Page, text: string)
+    {
+        return page.locator('tr:has-text("' + text + '")');
+    }
+
+    static async clickCmfiveNavbar(page: Page, category: string, option: string)
+    {
+        const navbarCategory = page.locator("#topnav_" + category.toLowerCase().split(" ").join("_"));
+        
+        const bootstrap5 = await this.isBootstrap5(page);
+        if(bootstrap5)
+            await navbarCategory.click();
+        else // Foundation
+            await navbarCategory.hover();
+
+        await navbarCategory.getByText(option).click();
+    }
+
+    // Call exactly once per test before any dialogs pop up
+    static async acceptDialog(page: Page)
+    {
+        page.on('dialog', dialog => dialog.accept());
+    }
+
+    static async fillDatePicker(page: Page, datePickerTitle: string, field: string, date: DateTime) {          
+        await page.getByText(datePickerTitle).click();
+        await page.keyboard.type(date.toFormat("ddLLyyyy"));
+
+        const expectedDate = date.toISODate();
+        if(expectedDate == null)
+            throw new Error("date.toISODate() returned null");
+        else
+            await expect(page.locator("#" + field)).toHaveValue(expectedDate);
+    }   
+
+    static async fillAutoComplete(page: Page, field: string, search: string, value: string) {
+        await page.locator('#acp_' + field).click();
+        await page.keyboard.type(search);
+        await page.locator('.ui-menu-item :text("' + value + '")').click();
+    }
+}
