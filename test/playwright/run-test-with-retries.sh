fb4d5011e555ddfbf6a33f43e899f5eefab72b9a
claire <102092516+dragonflyfree@users.noreply.github.com>
2023-12-12 02:36:59 +0000

    update playwright run test with retries script

--- test/playwright/run-test-with-retries.sh
+++ test/playwright/run-test-with-retries.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
 
-# The first argument is the module name
-MODULE=$1
-
-# The second argument is the reporter, with "github" as the default if not provided
-REPORTER=${2:-"github"}
-
-cd test/playwright
-npm run build
+MODULE=${1:-""}
+BROWSER=${2:-"chromium"}
+REPORTER=${3:-"github"}
 
 success=false
 
 for i in 1 2 3; do
-    npm run test --module="$MODULE" --reporter="$REPORTER"
+    npm run test --module=$MODULE --platform=$BROWSER --reporter=$REPORTER
+
     if [ $? -eq 0 ]; then
-        echo "$MODULE module tests succeeded on attempt $i"
-        success=true
-        break
+        if [ -z "$MODULE" ]; then echo "Attempt $i: tests succeeded"
+        else echo "Attempt $i for $MODULE: tests succeeded"
+        fi
+        
+        exit 0
     else
-        echo "Attempt $i for $MODULE module failed, running cleanup and retrying..."
+        if [ $i -eq 3 ]; then
+            if [ -z "$MODULE" ]; then echo "Tests failed after 3 attempts"
+            else echo "$MODULE module tests failed after 3 attempts"
+            fi
+
+            exit 1
+        fi
+
+        if [ -z "$MODULE" ]; then echo "Attempt $i: tests failed, running cleanup and retrying..."
+        else echo "Attempt $i for $MODULE: tests failed, running cleanup and retrying..."
+        fi
+
         npm run cleanup
     fi
 done
-
-if [ "$success" = false ]; then
-    echo "$MODULE module tests failed after 3 attempts"
-    exit 1
-fi
