c43e063ef5d81f81ed0b638908af6edb51020475
Matt Bell <matt@2pisoftware.com>
2024-09-06 16:26:06 +1000

    Add theme compile warning

--- .codepipeline/ec2/application-start.sh
+++ .codepipeline/ec2/application-start.sh
@@ -9,6 +9,29 @@ fi
 if [ "$COSINE_DEPLOYMENT_TYPE" == "docker" ]; then
     systemctl start cosine
 
+    # Read cosine config
+    source /etc/cosine/cosine.conf
+
+    if [ "$CORE_BRANCH" != "master" ]; then
+        # Theme will need compiling
+        
+        # # Copy theme from container to host
+        # echo "Copying theme from container to host"
+        # docker cp cosine:/var/www/html/system/templates/base /tmp/cmfive-theme
+        # cd /tmp/cmfive-theme
+        # echo "Compiling theme"
+        # docker run --rm \
+        #     -v $(pwd):/app \
+        #     -w /app \
+        #     node:20-alpine sh -c "npm ci && npm run production"
+        # # Copy the compiled theme back to the docker container
+        # echo "Copying compiled theme back to container"
+        # docker cp /tmp/cmfive-theme/. cosine:/var/www/html/system/templates/base
+
+        # NOTE: Commented out the above due to high system requirements
+        echo "Theme compilation needed but skipped"
+    fi
+
     exit 0
 fi
 
