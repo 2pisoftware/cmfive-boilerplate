fbfb69e411a047a355a74b3c26a5956e98ef03dc
DerekCrannaford <43056286+DerekCrannaford@users.noreply.github.com>
2024-10-04 13:12:59 +1000

    Initial cut WIP

---
+++ test/docker_run_tests.sh
@@ -0,0 +1,162 @@
+#!/bin/sh
+
+# Script will self manage errors:
+set +e
+
+# This script is used to run the Playwright tests in a container
+# usage: ./docker_run.sh
+# Or to refresh the Cmfive Docker containers before running the tests
+# Node version can be specified, WARNING Playwright&Cosine will not be expected to support all node versions!
+# usage: ./docker_run.sh --fresh
+# usage: ./docker_run.sh --node_ver 18
+
+freshen_all=""
+test_node_version=""
+
+while test $# != 0
+do
+    case "$1" in
+    --fresh) freshen_all=true ;;
+    --node_ver)
+        shift; test_node_version=$1 ;;
+    --) shift; break;;
+    esac
+    shift
+done
+
+# Bootstrap this script in the Playwright container
+if [ -z "$IS_PLAYWRIGHT_CONTAINER" ]; then
+    # Explicitly prep DB for testing:
+    echo "Backup and snaphsot DB with migrations"
+    docker exec -t cmfive bash -c "DB_HOST=mysql-8 DB_USERNAME=$DB_USERNAME DB_PASSWORD=$DB_PASSWORD DB_DATABASE=$DB_DATABASE DB_PORT=3306 php cmfive.php testDB setup"
+    DB_SETUP_EXIT_CODE=$?
+    if [ $DB_SETUP_EXIT_CODE -ne 0 ]; then
+        echo "Warning: Failed to setup test DB"
+        exit $DB_SETUP_EXIT_CODE
+    fi
+    echo "Success: Setup test DB"
+
+    # Get the directory of the script
+    TESTDIR=$(dirname -- "$( readlink -f -- "$0"; )";)
+    
+    # Go up one directory from DIR until docker-compose.yml is found
+    PROJECTDIR=$TESTDIR
+    while [ ! -f "$PROJECTDIR/docker-compose.yml" ]; do
+        PROJECTDIR="$(dirname "$PROJECTDIR")"
+        echo "PROJECTDIR: $PROJECTDIR"
+        echo "TESTDIR: $TESTDIR"
+        if [ "$PROJECTDIR" == "/" ]; then
+            echo "Panic: docker-compose.yml not found, be sure to run this script from the test directory"
+            exit 2
+        fi
+    done
+
+    # if the --fresh flag is set, refresh the Cmfive Docker containers
+    if [ ! -z $freshen_all ]; then
+        echo "Refreshing Cmfive Docker containers"
+        docker compose down -v
+        docker compose up -d --wait
+    fi
+    
+    # First, Run Unit Tests
+    docker exec -u cmfive cmfive sh -c "DB_HOST=mysql-8 DB_USERNAME=$DB_USERNAME DB_PASSWORD=$DB_PASSWORD DB_DATABASE=$DB_DATABASE DB_PORT=3306 php cmfive.php tests unit all; exit \$?"
+    UNIT_TESTS_EXIT_CODE=$?
+    if [ $UNIT_TESTS_EXIT_CODE -ne 0 ]; then
+        echo "Unit tests failed: $UNIT_TESTS_EXIT_CODE"
+        exit $UNIT_TESTS_EXIT_CODE
+    fi
+    echo "Unit tests passed"
+
+    docker build -t playwright-cosine -f $TESTDIR/test_service.Dockerfile $PROJECTDIR
+
+    docker run -it --rm \
+        -e IS_PLAYWRIGHT_CONTAINER=1 \
+        -e LANG=en_AU.UTF-8 \
+        -e LC_ALL=en_AU.UTF-8 \
+        -e TEST_NODE_VERSION=$test_node_version \
+        -v ms-playwright-data-cmfive:/ms-playwright \
+        -v $PROJECTDIR:/cmfive-boilerplate \
+        --ipc=host \
+        --network=host \
+        --cap-add=SYS_ADMIN \
+        playwright-cosine \
+        "/cmfive-boilerplate/test/docker_run_tests.sh"
+        
+    DOCKER_EXIT_CODE=$?
+
+    set -e
+
+    # OK, now we are done with the Playwright container
+    # Consider cleanup:
+    # We can hold the DB as-is, for further manual testing.
+    # But should clean up migration artefacts, to avoid git diff.
+    cd $TESTDIR/playwright
+    ./cleanupTestMigrations.sh --no-confirm
+    # but no: npm run cleanup
+    
+    if [ $DOCKER_EXIT_CODE -ne 0 ]; then
+        echo "Playwright container exited with code $DOCKER_EXIT_CODE"
+        exit $DOCKER_EXIT_CODE
+    fi
+
+    echo "Success: Playwright tests completed"
+    exit
+fi
+
+wait_for_response() {
+    local url=$1
+    local timeout=$2
+
+    echo "Waiting for $url to respond"
+    for i in $(seq 1 $timeout); do
+        if curl -s -o /dev/null -w "%{http_code}" "$url"; then
+            echo -n "."
+            break
+        fi
+        sleep 1
+    done
+
+    if [ $i -eq $timeout ]; then
+        echo "Failed to get a response from $url"
+        exit 1
+    fi
+    echo ""
+    echo "$url responded"
+}
+
+wait_for_response "http://localhost:3000" 60
+
+chmod -R 755 .
+whoami
+locale
+date
+
+
+# if TEST_NODE_VERSION is set, adopt version
+
+if [ ! -z $TEST_NODE_VERSION ]; then
+    echo "Switching node version"
+    n $TEST_NODE_VERSION
+    hash -r
+    node --version
+fi
+
+cd /cmfive-boilerplate/test/playwright
+npm i
+npm run build
+
+# Run the tests, recommended 1 worker for Playwright for memory usage
+export WORKERS=1
+
+# Retry failed tests, recommended 0 retries because of data consistency
+export RETRIES=0
+
+npm run test 
+PLAYWRIGHT_TESTS_EXIT_CODE=$?
+    if [ $PLAYWRIGHT_TESTS_EXIT_CODE -ne 0 ]; then
+        echo "Playwright tests failed: $PLAYWRIGHT_TESTS_EXIT_CODE"
+        exit $PLAYWRIGHT_TESTS_EXIT_CODE
+    fi
+
+echo "Playwright tests passed"
+
