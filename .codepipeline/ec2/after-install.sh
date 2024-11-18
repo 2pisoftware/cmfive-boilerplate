3c611843dd424898040c3daf09d72fe8ba76016d
DerekCrannaford <43056286+DerekCrannaford@users.noreply.github.com>
2024-11-18 14:00:40 +1100

    mv regardless of /html/ existence

--- .codepipeline/ec2/after-install.sh
+++ .codepipeline/ec2/after-install.sh
@@ -22,8 +22,10 @@ fi
 
 echo "Running After Install"
 
-# Rename directory.
-mv /var/www/cmfive-boilerplate /var/www/html
+# Shift built pipeline boilerplate-asset into hosted /html/
+mkdir -p /var/www/html
+mv /var/www/cmfive-boilerplate/* /var/www/html
+mv /var/www/cmfive-boilerplate/.[!.]* /var/www/html
 cd /var/www/html
 
 # Create symlink to system.
